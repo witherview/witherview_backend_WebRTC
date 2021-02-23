2d1701b3e83956cffb9a6f532bcd9d44fdf4783c
inspirit941 <inspirit941@gmail.com>
2021-02-23 12:28:58 +0900

    feat: disconnect 백엔드 로직 - (브라우저 종료 문제 미반영)

--- server.js
+++ server.js
@@ -72,20 +72,35 @@ io.on('connection', (sock) => {
     }
   });
 
+  // todo: 브라우저 종료할 경우의 종료 로직.
+  // 커넥션을 종료할 때
   sock.on('disconnect', () => {
-    if (
-      users[socketToRoom[sock.id]]
-      && users[socketToRoom[sock.id]].length <= 2
-    ) {
-      users[socketToRoom[sock.id]].forEach((val) => {
-        io.to(val).emit('refresh');
-      });
-      users[socketToRoom[sock.id]] = users[socketToRoom[sock.id]].filter(
-        (val) => val !== sock.id,
-      );
-    }
-    delete socketToRoom[sock.id];
+    let roomID = socketToRoom.get(sock.id);
+    if (roomID === undefined) { 
+      // todo: 해당 방에 존재하지 않는 커넥션이 들어올 때. 예외처리가 필요할까?
+      return;
+    } 
+    // 해당 방에 특정 유저가 떠났다는 user left 메시지를 보낸다.
+    sock.broadcast.to(roomID).emit("user left", sock.id);
+
+    // 서버에 저장된 데이터 삭제.
+    socketToRoom.delete(sock.id);
+    users[roomID].filter((id) => id !== sock.id);
   });
+  // sock.on('disconnect', () => {
+  //   if (
+  //     users[socketToRoom[sock.id]]
+  //     && users[socketToRoom[sock.id]].length <= 2
+  //   ) {
+  //     users[socketToRoom[sock.id]].forEach((val) => {
+  //       io.to(val).emit('refresh');
+  //     });
+  //     users[socketToRoom[sock.id]] = users[socketToRoom[sock.id]].filter(
+  //       (val) => val !== sock.id,
+  //     );
+  //   }
+  //   delete socketToRoom[sock.id];
+  // });
 });
 
 server.listen(process.env.PORT || 8000, () => console.log('server is running on port 8000'));
